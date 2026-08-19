54bff144880c6c9a450c71bbfb22a442a55a4b03
yashsh0018-web <yashsh0018@users.noreply.github.com>
2026-08-20 00:39:36 +0530

    fix(vercel): clean next.config.mjs configuration and un-track zip archive for Vercel deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    allowedDevOrigins: ['172.17.30.240:3000', 'localhost:3000'],
-  },
   images: {
     remotePatterns: [
       {
